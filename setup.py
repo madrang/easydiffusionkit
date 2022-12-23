865374c17c4490c07ac106a09ce8a38dd059fcfa
cmdr2 <secondary.cmdr2@gmail.com>
2022-12-23 13:06:18 +0530

    Remove the hacky dependency on torch and torchvision. Can't install it automatically in a pip install, users will have to install it manually

--- setup.py
+++ setup.py
@@ -1,31 +1,4 @@
 from setuptools import setup, find_packages
-import platform
-
-# hack inspired by https://github.com/pytorch/pytorch/issues/50718#issuecomment-1329360776
-def install_torch(package: str, version: str):
-    if platform.system() == "Darwin":
-        return f'{package}=={version}'
-
-    cuda_version = 'cu116'
-    python_version = ''.join(platform.python_version().split('.')[:2])
-    os = ''
-
-    include_cuda_in_filename = True
-    if platform.system() == "Windows":
-        os = 'win_amd64'
-    elif platform.system() == "Linux":
-        if platform.machine().lower() == 'aarch64':
-            os = 'manylinux2014_aarch64'
-            include_cuda_in_filename = False
-        else:
-            os = 'linux_x86_64'
-
-    if include_cuda_in_filename:
-        wheel_filename = f'{package}-{version}%2B{cuda_version}-cp{python_version}-cp{python_version}-{os}.whl'
-    else:
-        wheel_filename = f'{package}-{version}-cp{python_version}-cp{python_version}-{os}.whl'
-
-    return f'{package} @ https://download.pytorch.org/whl/{cuda_version}/{wheel_filename}'
 
 setup(
     name='stable-diffusion-sdkit',
@@ -33,9 +6,6 @@ setup(
     description='',
     packages=find_packages(),
     install_requires=[
-        install_torch('torch', '1.12.1'),
-        install_torch('torchvision', '0.13.1'),
-
         "albumentations==1.3.0",
         "opencv-python==4.6.0.66",
         "pytorch-lightning==1.4.2",
