d9eaa21b52cdc71411c80c63cbe938e7bae6621a
cmdr2 <secondary.cmdr2@gmail.com>
2022-12-23 15:02:24 +0530

    Package updates

--- setup.py
+++ setup.py
@@ -22,9 +22,6 @@ if platform.system() == 'Darwin':
 # They need to run: pip install torch torchvision --extra-index-url https://download.pytorch.org/whl/cu116
 
 setup(
-    name='stable-diffusion-sdkit',
-    version='2.1.0',
-    description='',
     packages=find_packages(),
     install_requires=dependencies,
 )
